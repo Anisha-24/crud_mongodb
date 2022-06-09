5d0d7cddf3f2ced17b1637c8e783ebe179b1cd45
Anisha-24 <anisha242002@gmail.com>
2022-06-09 12:29:29 +0530

    get_post_delete

---
+++ src/main/java/com/anisha/crud_mongodb/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package com.anisha.crud_mongodb.controller;
+
+import com.anisha.crud_mongodb.model.Employee;
+import com.anisha.crud_mongodb.repository.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepo repo;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/getEmployee")
+    public List<Employee> getEmployee(){
+        return repo.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/postEmployee")
+    public String postEmployee(@RequestBody Employee employee){
+        repo.save(employee);
+        return "Added Successfully";
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/delEmployee")
+    public String delEmployee(@PathVariable int id){
+        repo.deleteById(id);
+        return "Deleted Successfully";
+    }
+
+}
---
+++ src/main/java/com/anisha/crud_mongodb/model/Employee.java
@@ -0,0 +1,21 @@
+package com.anisha.crud_mongodb.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collation = "Employees")
+public class Employee {
+    @Id
+    private int id;
+    private int year;
+    private String name;
+    private String city;
+}
