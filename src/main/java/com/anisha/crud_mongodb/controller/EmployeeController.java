3dfb369583feec95863afd402dd75f7556e33cff
Anisha-24 <anisha242002@gmail.com>
2022-06-14 16:15:25 +0530

    get_post_delete_put

--- src/main/java/com/anisha/crud_mongodb/controller/EmployeeController.java
+++ src/main/java/com/anisha/crud_mongodb/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.anisha.crud_mongodb.controller;
 import com.anisha.crud_mongodb.model.Employee;
 import com.anisha.crud_mongodb.repository.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,4 +31,14 @@ public class EmployeeController {
         return "Deleted Successfully";
     }
 
+    @PutMapping(value = "/putEmployee/{id}")
+    public ResponseEntity<Employee> putEmployee(@PathVariable(value = "id") int Id, @RequestBody Employee employee){
+        Employee emp = repo.findById(Id);
+        employee.setCity(employee.getCity());
+        employee.setId(employee.getId());
+        employee.setName(employee.getName());
+        employee.setYear(employee.getYear());
+        final Employee updatedEmployee = repo.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+    }
 }
--- src/main/java/com/anisha/crud_mongodb/repository/EmployeeRepo.java
+++ src/main/java/com/anisha/crud_mongodb/repository/EmployeeRepo.java
@@ -4,4 +4,5 @@ import com.anisha.crud_mongodb.model.Employee;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface EmployeeRepo extends MongoRepository<Employee,Integer> {
+    Employee findById(int id);
 }
